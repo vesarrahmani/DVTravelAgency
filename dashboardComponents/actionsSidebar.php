36efd8d8a7e0924a9715c11d0e431ede1bac20f5
Dijane Hoti <dhotii002@gmail.com>
2023-02-21 11:14:44 +0100

    edit

--- dashboardComponents/actionsSidebar.php
+++ dashboardComponents/actionsSidebar.php
@@ -4,12 +4,12 @@
 </div>
 
 <hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-    <a href="./index.php" ><i class="fa fa-home"></i> Dashboard</a>
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">x</a>
+    <a href="../index.php" ><i class="fa fa-home"></i> Dashboard</a>
     <a href="usersDashboard.php"  onclick="showCustomers()" ><i class="fa fa-users"></i> Users</a>
     <a href="bookingsDashboard.php"   onclick="showCategory()" ><i class="fa fa-calendar"></i> Bookings</a>
   
-  <!---->
+  
 </div>
  
 <div id="main">
