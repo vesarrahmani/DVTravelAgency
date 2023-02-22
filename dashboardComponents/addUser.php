f9096f4c62d22ce7ab2f508386ec3b0890b742dd
Vesa Rrahmani <vesarrahmani17@gmail.com>
2023-02-23 00:29:00 +0100

    forgot to edit smth so edited just now :)

--- dashboardComponents/addUser.php
+++ dashboardComponents/addUser.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css code/dashboard.css?v=<?php echo time();?>">
-    <link rel="stylesheet" href="../css code/userDashboard.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="../css/dashboard.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="../css/userDashboard.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.0/css/all.min.css">
 
 
